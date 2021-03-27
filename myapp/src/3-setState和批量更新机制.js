4515eb5069b3ee30b736440d77526a7c9e5acdac
Unknown <unknown@example.com>
2021-03-27 13:08:38 +0800

    setState和批量更新机制：异步还是同步

--- myapp/src/3-setState和批量更新机制.js
+++ myapp/src/3-setState和批量更新机制.js
@@ -16,21 +16,31 @@ class App extends Component {
         this.setState({
           num: num + 5
         })
-        console.log(num, this.state.num)
-         // setState同步
+        console.log('222', this.state.num)
+         // 每个异步函数都会触发render
         setTimeout(()=>{
+          // setState同步
           this.setState({
-            count: count + 1
+            count: num + 5
           })
-          console.log(count, this.state.count)
+          console.log('333', this.state.count)
         })
       }}>点击</button>
       <button onClick={()=>{
+        // 同一个操作中，批量的setState操作，只触发一次render()
         // 触发了批量更新机制，setState为异步
         this.setState({
           count: count + 2
         })
         console.log(this.state.count)
+        this.setState({
+          count: count + 2
+        })
+        console.log(this.state.count)
+        this.setState({
+          num: num + 5
+        })
+        console.log(this.state.num)
         this.setState({
           num: num + 5
         })
